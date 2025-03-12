2bec74399d1dfc7fff6955afa603c9861c1f1d47
Seungmin0216 <7853654@naver.com>
2025-03-13 00:36:52 +0900

    first commit

--- test.cpp
+++ test.cpp
@@ -1,2 +1,12 @@
-test
-hello
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " III     L         OOO      V   V    EEEEE        L         SSSSS   M   M\n";
+    cout << "  I      L        O   O     V   V    E            L        S        MM MM\n";
+    cout << "  I      L        O   O     V   V    EEEE         L         SSSSS   M M M\n";
+    cout << "  I      L        O   O      V V     E            L             S   M   M\n";
+    cout << " III     LLLLL     OOO        V     EEEEE        LLLLL     SSSSS   M   M\n";
+    
+    return 0;
+}
